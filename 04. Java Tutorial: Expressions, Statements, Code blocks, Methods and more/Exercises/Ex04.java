8f416fe11d35722cf4cd954e13328e6cae2c6ea0
geekahmed <geekahmed1@gmail.com>
2022-06-18 00:34:48 +0200

    Section 4, Exercise 4

---
+++ 04. Java Tutorial: Expressions, Statements, Code blocks, Methods and more/Exercises/Ex04.java
@@ -0,0 +1,10 @@
+public class Ex04 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isLeapYear(int year){
+        if(year < 1 || year > 9999){
+            return false;
+        } else return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+}

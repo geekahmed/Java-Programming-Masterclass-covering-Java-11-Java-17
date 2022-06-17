d18376aec05d4fb656ad1cfea0ed08fec9d390ae
geekahmed <geekahmed1@gmail.com>
2022-06-18 00:49:04 +0200

    Section 4, Exercise 6

---
+++ 04. Java Tutorial: Expressions, Statements, Code blocks, Methods and more/Exercises/Ex06.java
@@ -0,0 +1,8 @@
+public class Ex06 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasEqualSum(int num1, int num2, int num3){
+        return (num1 + num2) == num3;
+    }
+}

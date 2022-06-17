fb215751e6e4aa701b4210b8fa8f6367b8d6043d
geekahmed <geekahmed1@gmail.com>
2022-06-17 23:47:40 +0200

    Changing folder structure

---
+++ 04. Java Tutorial: Expressions, Statements, Code blocks, Methods and more/Exercises/Ex1.java
@@ -0,0 +1,19 @@
+public class Ex1 {
+    public static void main(String[] args) {
+            printConversion(1.2);
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour < 0) System.out.println("Invalid Value");
+        else {
+            long roundedConversion = toMilesPerHour(kilometersPerHour);
+            System.out.printf("%f km/h = %d mi/h", kilometersPerHour, roundedConversion);
+        }
+    }
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour < 0) return -1;
+        
+        return Math.round(1.609 * kilometersPerHour);
+    }
+    
+}
---
+++ 04. Java Tutorial: Expressions, Statements, Code blocks, Methods and more/README.md
@@ -0,0 +1,1 @@
+# Section 4: Java Tutorial: Expressions, Statements, Code blocks, Methods and more

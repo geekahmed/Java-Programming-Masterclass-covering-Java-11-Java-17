d6a70e615c9f47b207fa1e691ff0871ad42136c9
geekahmed <geekahmed1@gmail.com>
2022-06-18 00:59:33 +0200

    Section 4, Exercise 8

---
+++ 04. Java Tutorial: Expressions, Statements, Code blocks, Methods and more/Exercises/Ex08.java
@@ -0,0 +1,14 @@
+public class Ex08 {
+    public static void main(String[] args) {
+
+    }
+
+    public static double area (double radius){
+        if (radius < 0) return -1.0;
+        return Math.PI * Math.pow(radius, 2);
+    }
+    public static double area (double x, double y){
+        if (x < 0 || y < 0) return -1.0;
+        return x * y;
+    }
+}

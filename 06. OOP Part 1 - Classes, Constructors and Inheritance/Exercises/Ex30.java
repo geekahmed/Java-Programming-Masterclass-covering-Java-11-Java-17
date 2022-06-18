27ab559aafc916f3abe6d2cff28c346fdf247800
geekahmed <geekahmed1@gmail.com>
2022-06-18 12:02:56 +0200

    Section 6, Exercises 29 and 30

---
+++ 06. OOP Part 1 - Classes, Constructors and Inheritance/Exercises/Ex30.java
@@ -0,0 +1,61 @@
+public class Ex30 {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) this.age = 0;
+        else this.age = age;
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "";
+        } else if (firstName.isEmpty()) {
+            return lastName;
+        } else if (lastName.isEmpty()) {
+            return firstName;
+        } else {
+            return firstName + " " + lastName;
+        }
+    }
+
+    public boolean isTeen() {
+        return age > 12 && age < 20;
+    }
+
+    public static void main(String[] args) {
+        Ex30 person = new Ex30();
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFirstName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");
+        System.out.println("fullName= " + person.getFullName());
+    }
+}
